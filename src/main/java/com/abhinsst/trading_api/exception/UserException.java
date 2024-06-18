ac4ae453ea7427a09afc15ee527a1490452640d9
anand9773 <aaanand9773@gmail.com>
2024-06-19 05:24:49 +0530

    project created

---
+++ src/main/java/com/abhinsst/trading_api/exception/UserException.java
@@ -0,0 +1,9 @@
+package com.abhinsst.trading_api.exception;
+
+public class UserException extends Exception {
+
+  UserException(String message) {
+    super(message);
+  }
+
+}
---
+++ src/test/java/com/abhinsst/trading_api/TradingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinsst.trading_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
